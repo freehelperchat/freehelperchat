9597b81ec8284d04912604afd2526408e63fc70f
cdias900 <pedro@cdias.dev>
2020-03-11 17:53:05 -0300

    Fix queue model

--- server/src/models/queue/Queue.js
+++ server/src/models/queue/Queue.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 
 const QueueSchema = new mongoose.Schema({
-    chatId = {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Chat',
-    },
-    time:{
-        type: Date,
-        required: true,
-    },
-    previousOperatosId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Operator',
-    },
-    nextOperatorId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Operator',
-    },
-    motivo:{
-        type: String
-    },
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chat',
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  previousOperatosId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Operator',
+  },
+  nextOperatorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Operator',
+  },
+  motivo: {
+    type: String,
+  },
 });
+
+mongoose.model('Queue', QueueSchema);
