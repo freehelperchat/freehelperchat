ff73844c95b0be7540ffe767263ab15ec8e073c4
cdias900 <pedro@cdias.dev>
2020-03-12 16:33:02 -0300

    Add sending chat messages

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 const routes = require('./server/routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 const port = process.env.PORT || 3001;
+
+const connectedOperators = {};
+
+io.on('connection', (socket) => {
+  const { operatorToken } = socket.handshake.query;
+  if (operatorToken) {
+    connectedOperators[operatorToken] = socket.id;
+  }
+
+  socket.on('open_chat', (data) => {
+    const { chatId } = data;
+    socket.join(chatId);
+  });
+
+  socket.on('send_message', (data) => {
+    const { chatId, message } = data;
+    socket.to(chatId).emit('received_message', message);
+    socket.emit('received_message', message);
+  });
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,4 +41,4 @@ app.get('/chat/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
