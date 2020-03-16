188412836878a69e798ca7f99ee8771de5eb4d1f
cdias900 <pedro@cdias.dev>
2020-03-16 17:47:50 -0300

    Add select input type

--- client/src/components/input/Input.js
+++ client/src/components/input/Input.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
 const Input = props => {
-  return (
-    <div>
-      <label htmlFor={props.Id}>{props.Label}</label>
-      <input type={props.Type} onChange={props.Change} id={props.Id} />
-    </div>
-  );
+  let input = null;
+  switch (props.Type) {
+    case 'select':
+      input = (
+        <select
+          defaultValue=""
+          onChange={props.Change}
+          required={props.Required}
+        >
+          <option value="">{props.Label}</option>
+          {props.Options.map(op => (
+            <option key={op} value={op}>
+              {op}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+
+    default:
+      input = (
+        <>
+          <label htmlFor={props.Name}>{props.Label}</label>
+          <input
+            type={props.Type}
+            onChange={props.Change}
+            id={props.Name}
+            required={props.Required}
+          />
+        </>
+      );
+      break;
+  }
+  return <div>{input}</div>;
 };
 
 export default Input;
